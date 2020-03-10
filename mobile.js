fbe65ac69f94165dc3b12791e64de6c999a0d7da
alihelpervi <61271872+alihelpervi@users.noreply.github.com>
2020-03-10 10:21:53 +0200

    Create mobile.js

---
+++ mobile.js
@@ -0,0 +1,7 @@
+(function() {
+    var parent = document.getElementsByTagName('head').item(0);
+    var style = document.createElement('style');
+    style.type = 'text/css';
+    style.src = "https://raw.githubusercontent.com/alihelpervi/webview/master/mobile.css";
+    parent.appendChild(style);
+})();
